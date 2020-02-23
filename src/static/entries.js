f0671132b17dca547ce862b34f09e4a591b564cf
abdulwahab sahyoun <abdu_sah@hotmail.com>
2020-02-23 23:31:57 +0400

    expo sdk update

--- src/static/entries.js
+++ src/static/entries.js
@@ -1,78 +1,78 @@
 export const CAT_ENTRIES = [
   {
-    title: 'Whatever',
-    subtitle: '',
+    title: "Whatever",
+    subtitle: "",
     illustration:
-      'https://orig00.deviantart.net/41fb/f/2012/351/a/2/random_character_1_by_mnrart-d5odgq0.gif',
+      "https://404store.com/2017/12/08/random_character_1_by_mnrart-d5odgq0.gif",
     unlocked_illustration:
-      'https://orig00.deviantart.net/41fb/f/2012/351/a/2/random_character_1_by_mnrart-d5odgq0.gif',
-    value: ''
+      "https://404store.com/2017/12/08/random_character_1_by_mnrart-d5odgq0.gif",
+    value: ""
   },
   {
-    title: 'Video Games',
-    subtitle: '',
+    title: "Video Games",
+    subtitle: "",
     illustration:
-      'https://az616578.vo.msecnd.net/files/2016/05/20/6359938359335732161481727208_video%20games.jpg',
+      "https://imgix.bustle.com/uploads/image/2019/12/12/ed336be3-2ce1-4f50-9f73-967b0c9bd6d7-gettyimages-1129878609.jpg?w=1020&h=576&fit=crop&crop=faces&auto=format&q=70",
     unlocked_illustration:
-      'https://az616578.vo.msecnd.net/files/2016/05/20/6359938359335732161481727208_video%20games.jpg',
+      "https://imgix.bustle.com/uploads/image/2019/12/12/ed336be3-2ce1-4f50-9f73-967b0c9bd6d7-gettyimages-1129878609.jpg?w=1020&h=576&fit=crop&crop=faces&auto=format&q=70",
     value: 15
   },
   {
-    title: 'History',
-    subtitle: '',
+    title: "History",
+    subtitle: "",
     illustration:
-      'https://www.thegreatcourses.com/media/catalog/product/cache/1/image/800x600/0f396e8a55728e79b48334e699243c07/3/4/3450---base_image.1488405598.jpg',
+      "https://aralingua.com/wp-content/uploads/2019/03/History-of-Arabic-Translation-in-Arab-World.jpg",
     unlocked_illustration:
-      'https://www.thegreatcourses.com/media/catalog/product/cache/1/image/800x600/0f396e8a55728e79b48334e699243c07/3/4/3450---base_image.1488405598.jpg',
+      "https://aralingua.com/wp-content/uploads/2019/03/History-of-Arabic-Translation-in-Arab-World.jpg",
     value: 23
   },
   {
-    title: 'Animals',
-    subtitle: '',
-    illustration: 'https://i.imgur.com/orExM11.jpg',
-    unlocked_illustration: 'https://i.imgur.com/orExM11.jpg',
+    title: "Animals",
+    subtitle: "",
+    illustration: "https://i.imgur.com/orExM11.jpg",
+    unlocked_illustration: "https://i.imgur.com/orExM11.jpg",
     value: 27
   },
   {
-    title: 'Geography',
-    subtitle: '',
+    title: "Geography",
+    subtitle: "",
     illustration:
-      'https://carleton.ca/geography/wp-content/uploads/Geography-Banner.jpg',
+      "https://carleton.ca/geography/wp-content/uploads/Geography-Banner.jpg",
     unlocked_illustration:
-      'https://carleton.ca/geography/wp-content/uploads/Geography-Banner.jpg',
+      "https://carleton.ca/geography/wp-content/uploads/Geography-Banner.jpg",
     value: 22
   },
   {
-    title: 'Computers',
-    subtitle: '',
+    title: "Computers",
+    subtitle: "",
     illustration:
-      'https://assets.materialup.com/uploads/b0597a97-8347-4f9c-b6c2-0bdefa982d44/preview.jpg',
+      "https://assets.materialup.com/uploads/b0597a97-8347-4f9c-b6c2-0bdefa982d44/preview.jpg",
     unlocked_illustration:
-      'https://assets.materialup.com/uploads/b0597a97-8347-4f9c-b6c2-0bdefa982d44/preview.jpg',
+      "https://assets.materialup.com/uploads/b0597a97-8347-4f9c-b6c2-0bdefa982d44/preview.jpg",
     value: 18
   },
   {
-    title: 'Science & Nature',
-    subtitle: '',
-    illustration: 'https://i.imgur.com/sduLRvf.jpg',
-    unlocked_illustration: 'https://i.imgur.com/sduLRvf.jpg',
+    title: "Science & Nature",
+    subtitle: "",
+    illustration: "https://i.imgur.com/sduLRvf.jpg",
+    unlocked_illustration: "https://i.imgur.com/sduLRvf.jpg",
     value: 17
   },
   {
-    title: 'Books',
-    subtitle: '',
-    illustration: 'https://i.imgur.com/C3MzO1Y.png',
+    title: "Books",
+    subtitle: "",
+    illustration: "https://i.imgur.com/C3MzO1Y.png",
     unlocked_illustration:
-      'https://i.ebayimg.com/images/g/l6IAAOxyUrZS64di/s-l300.jpg',
+      "https://i.ebayimg.com/images/g/l6IAAOxyUrZS64di/s-l300.jpg",
     value: 10,
     unlockPoints: 25
   },
   {
-    title: 'Film',
-    subtitle: '',
-    illustration: 'https://i.imgur.com/Y6Jml4L.png',
+    title: "Film",
+    subtitle: "",
+    illustration: "https://i.imgur.com/Y6Jml4L.png",
     unlocked_illustration:
-      'https://static1.squarespace.com/static/55396962e4b053ce148cb1e1/t/57210fcab6aa60cca01f49fb/1461784525042/rolleiflex-brooklyn-film-camera.jpg?format=1500w',
+      "https://static1.squarespace.com/static/55396962e4b053ce148cb1e1/t/57210fcab6aa60cca01f49fb/1461784525042/rolleiflex-brooklyn-film-camera.jpg?format=1500w",
     value: 11,
     unlockPoints: 35
   }
@@ -80,31 +80,31 @@ export const CAT_ENTRIES = [
 
 export const DIFF_ENTRIES = [
   {
-    title: 'Any',
-    subtitle: '',
-    illustration: 'http://i.imgur.com/SsJmZ9jl.jpg',
-    unlocked_illustration: 'http://i.imgur.com/SsJmZ9jl.jpg',
-    value: ''
+    title: "Any",
+    subtitle: "",
+    illustration: "http://i.imgur.com/SsJmZ9jl.jpg",
+    unlocked_illustration: "http://i.imgur.com/SsJmZ9jl.jpg",
+    value: ""
   },
   {
-    title: 'Easy',
-    subtitle: '',
-    illustration: 'http://i.imgur.com/SsJmZ9jl.jpg',
-    unlocked_illustration: 'http://i.imgur.com/SsJmZ9jl.jpg',
-    value: 'easy'
+    title: "Easy",
+    subtitle: "",
+    illustration: "http://i.imgur.com/SsJmZ9jl.jpg",
+    unlocked_illustration: "http://i.imgur.com/SsJmZ9jl.jpg",
+    value: "easy"
   },
   {
-    title: 'Medium',
-    subtitle: '',
-    illustration: 'http://i.imgur.com/5tj6S7Ol.jpg',
-    unlocked_illustration: 'http://i.imgur.com/5tj6S7Ol.jpg',
-    value: 'medium'
+    title: "Medium",
+    subtitle: "",
+    illustration: "http://i.imgur.com/5tj6S7Ol.jpg",
+    unlocked_illustration: "http://i.imgur.com/5tj6S7Ol.jpg",
+    value: "medium"
   },
   {
-    title: 'Hard',
-    subtitle: '',
-    illustration: 'http://i.imgur.com/pmSqIFZl.jpg',
-    unlocked_illustration: 'http://i.imgur.com/pmSqIFZl.jpg',
-    value: 'hard'
+    title: "Hard",
+    subtitle: "",
+    illustration: "http://i.imgur.com/pmSqIFZl.jpg",
+    unlocked_illustration: "http://i.imgur.com/pmSqIFZl.jpg",
+    value: "hard"
   }
 ];
